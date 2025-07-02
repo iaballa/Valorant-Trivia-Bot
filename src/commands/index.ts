713fa074a6ecc67d704ea1f070c6b5ccfff4aae6
iaballa <iaballa@iba-macbook-pro.lan>
2025-07-02 16:43:49 -0700

    adding files

---
+++ src/commands/index.ts
@@ -0,0 +1,7 @@
+import * as trivia from "./trivia";
+import * as ping from "./ping";
+
+export const commands = {
+  trivia,
+  ping,
+};
---
+++ src/config.ts
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { DISCORD_TOKEN, DISCORD_CLIENT_ID } = process.env;
+
+if (!DISCORD_TOKEN || !DISCORD_CLIENT_ID) {
+  throw new Error("Missing Discord Token and Client ID");
+}
+
+export const config = {
+  DISCORD_TOKEN,
+  DISCORD_CLIENT_ID,
+};
